51b4a277096eadbe70847d437ee99108460ddb0c
OluhleM <222419636@mycput.ac.za>
2025-03-18 16:44:17 +0200

    This is the Payment and Appointment domain and factory.

---
+++ CapProject/CapstoneProject-main/PetCare/src/main/java/za/ac/cput/domain/Payment.java
@@ -0,0 +1,49 @@
+package za.ac.cput.domain;
+
+public class Payment {
+    private double amount;
+    private String paymentType;
+
+    public Payment() {}
+
+    public Payment(Builder builder){
+        this.amount = builder.amount;
+        this.paymentType = builder.paymentType;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "amount=" + amount +
+                ", paymentType='" + paymentType + '\'' +
+                '}';
+    }
+
+
+
+    public static class Builder {
+        private double amount;
+        private String paymentType;
+
+        public Builder setAmount(double amount) {
+            this.amount = amount;
+            return this;
+        }
+
+        public Builder setPaymentType(String paymentType) {
+            this.paymentType = paymentType;
+            return this;
+        }
+
+        public Payment build() {return new Payment(this); }
+    }
+
+}
