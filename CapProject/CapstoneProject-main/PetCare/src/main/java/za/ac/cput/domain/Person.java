0221b146900544cda2c7552ed227e90b58909894
OluhleM <222419636@mycput.ac.za>
2025-03-18 16:55:27 +0200

    This is the Payment and Appointment domain and factory.

---
+++ CapProject/CapstoneProject-main/PetCare/src/main/java/za/ac/cput/domain/Person.java
@@ -0,0 +1,61 @@
+package za.ac.cput.domain;
+
+public class Person {
+    private String name;
+    private Contact contact;
+    private Address address;
+
+    private Person(Builder builder) {
+        this.name = builder.name;
+        this.contact = builder.contact;
+        this.address = builder.address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    // Builder Class
+    public static class Builder {
+        private String name;
+        private Contact contact;
+        private Address address;
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setContact(Contact contact) {
+            this.contact = contact;
+            return this;
+        }
+
+        public Builder setAddress(Address address) {
+            this.address = address;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                ", name='" + name + '\'' +
+                ", contact=" + contact +
+                ", address=" + address +
+                '}';
+    }
+}
+
