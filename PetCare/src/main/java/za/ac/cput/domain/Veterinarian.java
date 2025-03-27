722824727680638d7baddb3069ac1ea1317bf956
AthieX-34 <165181664+AthieX-34@users.noreply.github.com>
2025-03-27 15:20:14 +0200

    Add files via upload

---
+++ PetCare/src/main/java/za/ac/cput/domain/Veterinarian.java
@@ -0,0 +1,75 @@
+//Athenkosi Mekana
+//222319437
+//Veterinarian Domain Class
+
+package za.ac.cput.domain;
+import java.util.List;
+
+public class Veterinarian {
+
+    private int veterinarianId;
+
+    private List appointments;
+
+    private Veterinarian() {
+    }
+    private Veterinarian(Builder builder) {
+        this.veterinarianId = builder.veterianrianId;
+        this.appointments = builder.appointments;
+
+}
+public int getVeterinarianId() {
+    return veterinarianId;
+}
+
+    public List getAppointments() {
+        return appointments;
+    }
+
+    @Override
+    public String toString() {
+        return "Veterinarian{" +
+                "veterinarianId=" + veterinarianId +
+                ", appointments=" + appointments +
+                '}';
+    }
+    public static class Builder {
+        private int veterianrianId;
+        private List appointments;
+
+
+        public Builder setVeterinarianId(int veterinarianId) { //remove void place Builder to return Builder object
+            this.veterianrianId = veterinarianId;
+            return this;
+        }
+
+        public Builder setAppointments(List appointments) {
+            this.appointments = appointments;
+            return this;
+
+        }
+
+        public Builder copy(Veterinarian veterinarian) {
+            this.veterianrianId = veterinarian.veterinarianId;
+            this.appointments = veterinarian.appointments;
+            return this;
+
+        }
+        public Veterinarian build() {
+            return new Veterinarian(this);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
