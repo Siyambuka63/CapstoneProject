4538c7124177031481af2b11893a0688269b5f19
AthieX-34 <165181664+AthieX-34@users.noreply.github.com>
2025-03-28 20:55:56 +0200

    Add files via upload

---
+++ PetCare/src/test/java/za/ac/cput/factory/VeterinarianFactoryTest.java
@@ -0,0 +1,65 @@
+package za.ac.cput.factory;
+import org.junit.jupiter.api.*;
+import za.ac.cput.domain.Veterinarian;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
+class VeterinarianFactoryTest {
+
+
+
+    @Test
+
+    public void testCreateVeterinarian() {
+        int veterinarianId = 1;
+        List<String> appointments = new ArrayList<>();
+        appointments.add("2025-03-28 10:00");
+        Veterinarian veterinarian = VeterinarianFactory.createVeterinarian(veterinarianId, appointments);
+        assertNotNull(veterinarian);
+        assertEquals(veterinarianId, veterinarian.getVeterinarianId());
+    }
+    @Test
+    public void testCreateVeterinarianWithValidId() {
+        int veterinarianId = 6554;
+        List<String> appointments = new ArrayList<>();
+        appointments.add("2025-03-28 14:00");
+        Veterinarian veterinarian = VeterinarianFactory.createVeterinarian(veterinarianId, appointments);
+        assertNotNull(veterinarian);
+        assertEquals(6554, veterinarian.getVeterinarianId());
+
+
+    }
+    @Test
+    public void testCreateVeterinarianWithInvalidId() {
+        int veterinarianId = 0; // Invalid ID
+        List<String> appointments = new ArrayList<>();
+        Veterinarian veterinarian = VeterinarianFactory.createVeterinarian(veterinarianId, appointments);
+        assertNull(veterinarian);
+    }
+    @Test
+    public void createVeterinarianWithAppointments() {
+        int veterinarianId = 2;
+        List<String> appointments = new ArrayList<>();
+        appointments.add("2025-03-29 09:00");
+        appointments.add("2025-03-30 11:00");
+        Veterinarian veterinarian = VeterinarianFactory.createVeterinarian(veterinarianId, appointments);
+        assertNotNull(veterinarian);
+        assertEquals(2, veterinarian.getAppointments().size());
+    }
+    @Test
+    public void testCreateVeterinarianWithInvalidAppointments() {
+    fail();
+    }
+    @Test
+    @Disabled
+    public void testNotImplemented() {
+
+    }
+
+}
