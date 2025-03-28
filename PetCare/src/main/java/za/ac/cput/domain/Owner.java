74a71525d7b8a252480496ce2d4fe76361ce9891
Elethu427 <221195521@mycput.ac.za>
2025-03-28 20:48:59 +0200

    Owner

--- PetCare/src/main/java/za/ac/cput/domain/Owner.java
+++ PetCare/src/main/java/za/ac/cput/domain/Owner.java
@@ -1,6 +1,50 @@
 package za.ac.cput.domain;
 
 public class Owner {
-    //ToDo Add code
+    private String pets;
+    private Integer index;
+
+    private Owner() {
+    }
+
+    private Owner(Builder builder) {
+        this.pets = builder.pets;
+        this.index = builder.index;
+    }
+
+    public String getPets() {
+        return pets;
+    }
+
+    public Integer getIndex() {
+        return index;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Owner{" +
+                "pets='" + pets + '\'' +
+                ", index=" + index +
+                '}';
+    }
+
+    public static class Builder {
+        private String pets;
+        private Integer index;
+
+        public Builder setPets(String pets) {
+            this.pets = pets;
+            return this;
+        }
+        public Builder setIndex(Integer index) {
+            this.index = index;
+            return this;
+        }
+
+        public Owner build() {
+            return new Owner(this);
+        }
+    }
 
 }
--- PetCare/src/main/java/za/ac/cput/factory/OwnerFactory.java
+++ PetCare/src/main/java/za/ac/cput/factory/OwnerFactory.java
@@ -5,13 +5,13 @@ import za.ac.cput.util.Helper;
 
     public class OwnerFactory {
         public static Owner createPets(String pets) {
-            if (Helper.isNullorEmpty(pets))
+            if (Helper.isNullOrEmpty(pets))
                 return null;
 
-//    public static Owner removePets(String pets) {
-//        if (!Helper.isNullorEmpty(pets))
-//            return null;
-//        }
+  //  public static Owner removePets(String pets) {
+  //      if (!Helper.isNullOrEmpty(pets))
+  //          return null;
+  //      }
             return new Owner.Builder()
                     .setPets(pets)
                     .build();
